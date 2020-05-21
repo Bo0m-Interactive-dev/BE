4b27c2073189409c7c2f92501be5388379ee56c1
Gamelounch <58276955+Gamelounch@users.noreply.github.com>
2020-05-21 19:20:20 +0200

    gp

---
+++ Bo0m Engine/Source/EntryPoint.h
@@ -0,0 +1,9 @@
+/*    Copyright (c) 2020, Bo0m Interactive Inc.
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    https://github.com/Bo0m-Interactive-dev/BE/blob/master/LICENSE.
+ */
