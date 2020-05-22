f2cdcee6c74ffda505070bbda8e4355976307701
Gamelounch <58276955+Gamelounch@users.noreply.github.com>
2020-05-22 13:22:01 +0200

    Test Files Created

---
+++ Bo0m Engine/Source/Test.cpp
@@ -0,0 +1,11 @@
+#include "Test.h"
+#include "stdio.h"
+
+namespace BE
+{
+	void Print()
+	{
+		printf("Welcome to Bo0m Engine\n");
+	}
+
+}
---
+++ Bo0m Engine/Source/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+namespace BE
+{
+	__declspec(dllexport) void Print();
+	
+}
