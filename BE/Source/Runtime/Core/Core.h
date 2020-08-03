49cf89a54fdbfb8cac40d485fefff5799a96b76e
Gamelounch <58276955+Gamelounch@users.noreply.github.com>
2020-08-03 14:07:57 +0200

    Update Core.h

--- BE/Source/Runtime/Core/Core.h
+++ BE/Source/Runtime/Core/Core.h
@@ -1,2 +1,13 @@
 //Copyright 2020 Bo0m Interactive. All Rights Reserved
+#pragma once
+
+#ifdef BE_PLATFORM_WINDOWS
+	#ifdef BE_BUILD_DLL
+#define ENGINE_API __declspec(dllexport)
+	#else
+		#define ENGINE_API __declspec(dllimport)
+	#endif
+#else 
+	#error Bo0m Engine Only Supports Windows
+#endif
 
