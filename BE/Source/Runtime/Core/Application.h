94b7a516fb6bd7ad10ce3c72c3b6f58e1ec721a2
Gamelounch <58276955+Gamelounch@users.noreply.github.com>
2020-08-03 14:08:13 +0200

    Create Application.h

---
+++ BE/Source/Runtime/Core/Application.h
@@ -0,0 +1,20 @@
+#pragma once
+#include"Core.h"
+
+
+namespace BE {
+	class ENGINE_API Application
+	{
+
+	public:
+		Application();
+
+
+		virtual ~Application();
+
+		void Run();
+	};
+
+	//To be definet in CLIENT
+	Application* CreateApplication();
+}
