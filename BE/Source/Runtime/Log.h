0ee7cd27c8c9bf4b648a92a67810b8acc179a611
Gamelounch <58276955+Gamelounch@users.noreply.github.com>
2020-08-03 17:04:15 +0200

    Create Log.h

---
+++ BE/Source/Runtime/Log.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Core/Core.h"
+#include "../../VENDOR/spdlog/include/spdlog/logger.h"
+#include"memory.h"
+
+namespace BE
+{
+	class ENGINE_API Log
+	{
+	public:
+		static void init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger;  }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+
+	};
+}
